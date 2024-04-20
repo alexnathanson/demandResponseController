4abc910a6f55d496443303d70c3566a1ff50e4b3
Nathanson <nathaa@win.rpi.edu>
2024-04-19 20:26:22 -0400

    digital loggers relay example

--- components/DigitalLoggers.py
+++ components/DigitalLoggers.py
@@ -10,3 +10,22 @@ L___    ____G
      |
 '''
 
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+gPin = 18
+
+GPIO.setup(gPin, GPIO.OUT)
+
+
+GPIO.output(gPin, GPIO.HIGH)
+time.sleep(2)
+GPIO.output(gPin, GPIO.LOW)
+time.sleep(2)
+GPIO.output(gPin, GPIO.HIGH)
+time.sleep(2)
+GPIO.output(gPin, GPIO.LOW)
+
+GPIO.cleanup()
