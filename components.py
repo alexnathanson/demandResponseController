cad31f9daf216fd0634c45724e1509409fe3922f
Nathanson <nathaa@win.rpi.edu>
2024-04-19 22:58:28 -0400

    bleutti test

--- components.py
+++ components.py
@@ -4,15 +4,19 @@ import busio
 import adafruit_ina219
 import adafruit_ina260
 import asyncio
+from bluetti_mqtt.bluetooth import (
+    check_addresses, scan_devices, BluetoothClient, ModbusError,
+    ParseError, BadConnectionError)
 
 class Component:
-	def __init__(self, ina260=False, ina219=False, dl=False, kasa=False, ct=False):
+	def __init__(self, ina260=False, ina219=False, dl=False, kasa=False, ct=False, mac):
 		self.digitalloggers = dl
 		self.kasa = kasa
 		self.ct = ct
 		self.i2c = busio.I2C(board.SCL,board.SDA)
 		self.ina219 = adafruit_ina219.INA219(self.i2c)
 		self.ina260 = adafruit_ina260.INA260(self.i2c)
+		self.mac = mac
 
 
 	async def ina219Get(self):
@@ -27,3 +31,30 @@ class Component:
 	        "Current: %.2f mA Voltage: %.2f V Power:%.2f mW"
 	        % (self.ina260.current, self.ina260.voltage, self.ina260.power)
 	    )
+
+	# async def getData(self):
+	#     myData={
+	#     }
+
+	#     devices = await check_addresses({self.mac})
+	#     if len(devices) == 0:
+	#         sys.exit('Could not find the given device to connect to')
+	#     device = devices[0]
+
+	#     print(f'Connecting to {device.address}')
+	#     client = BluetoothClient(device.address)
+	#     asyncio.get_running_loop().create_task(client.run())
+
+	#     # Wait for device connection
+	#     while not client.is_ready:
+	#         print('Waiting for connection...')
+	#         await asyncio.sleep(1)
+	#         continue
+
+	#     # Poll device
+	#     #while True:
+	#     for command in device.logging_commands:
+	#         commandResponse = await log_command(client, device, command)
+	#         for k,v in commandResponse.items():
+	#             print(k + ": " + str(v))
+	#             myData[k]=v
--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 # 
 from components import Component
 import asyncio
+import components.Bluetti.AC180
 
 # frequency of logging in minutes
 updateRate = 1
@@ -8,12 +9,16 @@ updateRate = 1
 #async def getBluetti():
 
 #async def getINA219():
+myMac = 'DC:8A:6F:FD:79:66'
 
 async def main():
-	component = Component()
+	component = Component(myMac)
+
+
 	while True:
 		await component.ina219Get()
-		asyncio.sleep(5)
+		await getData(myDevice)
+		time.sleep(5)
 
 if __name__ == "__main__":
     asyncio.run(main())
