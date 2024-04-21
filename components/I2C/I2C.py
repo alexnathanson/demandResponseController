93becbdeaf85c944a531ab25b14c770add634da5
Nathanson <nathaa@win.rpi.edu>
2024-04-21 14:21:00 -0400

    both sensors

--- components/I2C/I2C.py
+++ components/I2C/I2C.py
@@ -16,9 +16,10 @@ import time
 import board
 import busio
 import adafruit_ina260
+import adafruit_ina219
+
 #from adafruit_bus_device.i2c_device import I2CDevice
 
-#i2c = busio.I2C(board.SCL, board.SDA)
 #print(dir(i2c))
 #ina260 = adafruit_ina260.INA260(i2c,0x2C)
 #ina260 = I2CDevice(i2c,0x2C)
@@ -35,12 +36,12 @@ REGISTER_SIZE = 2 #Number of bytes to read from each register
 
 # Initialize
 i2c = busio.I2C(board.SCL, board.SDA)
+
 while not i2c.try_lock():
     pass
 
-def ina260_device(i,adr):
-    return adafruit_ina260.INA260(i, adr)
-
+# def ina260_device(i,adr):
+#     return adafruit_ina260.INA260(i, adr)
 
 print('scanning I2C devices...')
 devices = i2c.scan()
@@ -48,13 +49,23 @@ while len(devices) < 1:
     print('scnaning I2C devices...')
     devices = i2c.scan()
 
-for d in devices:
-    print(hex(d))
-    ina260 = ina260_device(i2c,hex(d))
+[print(hex(d)) for d in devices]
+
+ina260 = adafruit_ina260.INA260(i2c,0x44)
+ina219 = adafruit_ina219.INA219(i2c,0x40)
+
 
 while len(devices) >= 1:
+    print('***  260 ***')
     print(
         "Current: %.2f mA Voltage: %.2f V Power:%.2f mW"
         % (ina260.current, ina260.voltage, ina260.power)
     )
+
+    print('*** 219 ***')
+    print("Bus Voltage: {} V".format(ina219.bus_voltage))
+    print("Shunt Voltage: {} mV".format(ina219.shunt_voltage / 1000))
+    print("Current: {} mA".format(ina219.current))
+    print("Power: {} W".format(ina219.power))
+
     time.sleep(2)
