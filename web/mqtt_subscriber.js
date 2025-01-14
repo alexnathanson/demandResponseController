bea38fc523b14bce3b799885a4caf9e94cd0340b
AlexNathanson <alex@alexnathanson.com>
2025-01-13 22:20:37 -0500

    eod

--- web/mqtt_subscriber.js
+++ web/mqtt_subscriber.js
@@ -3,11 +3,13 @@
 
 let port = 8080
 let BROKER = "test.mosquitto.org"
-let pChan = "OpenDemandResponse/Participant/AlexN"
-let aChan = "OpenDemandResponse/Event/BoroughHall"
-let participants = "OpenDemandResponse/participants"
+let pChan = "OpenDemandResponse/Participant/" //path for participants
+let pList = [];//['AlexN']; //list of participants
+let aChan = "OpenDemandResponse/Event/BoroughHall" //aggregator channel
+let partChan = "OpenDemandResponse/participants" //channel for participants to broadcast their own path
+
 // Create a client instance
-var client = new Paho.MQTT.Client(BROKER, Number(port), "clientId");
+let client = new Paho.MQTT.Client(BROKER, Number(port), "clientId");
 
 // set callback handlers
 client.onConnectionLost = onConnectionLost;
@@ -21,8 +23,10 @@ client.connect({onSuccess:onConnect});
 function onConnect() {
   // Once a connection has been made, make a subscription and send a message.
   console.log("onConnect");
-  client.subscribe(participants);
-  client.subscribe(pChan);
+  client.subscribe(partChan);
+  for (let p = 0; p < pList.length; p++){
+      client.subscribe(pChan + p);
+  }
   // message = new Paho.MQTT.Message("Hello");
   // message.destinationName = "World";
   // client.send(message);
@@ -37,5 +41,26 @@ function onConnectionLost(responseObject) {
 
 // called when a message arrives
 function onMessageArrived(message) {
-  console.log("onMessageArrived:"+message.payloadString);
+  console.log(message.destinationName)
+  console.log(pList)
+  if (message.destinationName == partChan){
+    //check if participant is new, and if so subscribe
+    if (!pList.includes(message.payloadString)){
+      pList.push(message.payloadString)
+      document.getElementById("participants").innerHTML = pList;
+
+      client.subscribe(pChan + message.payloadString);
+    }
+  } else {
+    let values = ['battery', 'ac_out', 'ac_in', 'dc_out', 'dc_in', 'r1', 'pv', 'rpi', 'load', 'timestamp']
+    let units = ['%', 'W', 'W', 'W', 'W', '', 'W', 'W', 'W','']
+    data = message.payloadString.split("#");
+    for (let v = 0; v < values.length;v++){
+      updatePage(values[v],data[v], units[v])
+    }
+  }
+}
+
+function updatePage(id,d, u){
+  document.getElementById(id).innerHTML = id + ": " + d + " " + u;
 }
