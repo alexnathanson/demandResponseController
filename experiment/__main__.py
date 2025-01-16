af863d539d4dcc6a018879ff22695eeaa61e1abf
Alex Nathanson <alex@alexnathanson.com>
2025-01-16 14:42:18 -0500

    exp file path

--- experiment/__main__.py
+++ experiment/__main__.py
@@ -1,7 +1,7 @@
 # run from directory above
 
 import sys
-sys.path.append('../demandResponseController')
+sys.path.append('..')
 from demandResponseController.componentClasses.components import DigitalLogger as DL
 from demandResponseController.componentClasses.components import INA
 from demandResponseController.componentClasses.currentTransformer import Current_Transformer as CT 
