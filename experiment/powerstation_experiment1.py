994ed7be75505de0097fff362eba89ca52c32439
Alex Nathanson <alex@alexnathanson.com>
2025-01-16 16:49:08 -0500

    packaging exception

--- experiment/powerstation_experiment1.py
+++ experiment/powerstation_experiment1.py
@@ -71,27 +71,30 @@ async def log(freq):
 
 def packageData(data):
 	print("packaging data...")
-	
-	pData = {}
+	try:
+		pData = {}
 
-	pData['battery'] = data['Power Station']['total_battery_percent']
-	pData['ac_out'] = data['Power Station']['ac_output_power']
-	pData['ac_in'] = data['Power Station']['ac_input_power']
-	pData['dc_out'] = data['Power Station']['dc_output_power']
-	pData['dc_in'] = data['Power Station']['dc_input_power']
-	pData['r1'] = data['R1']
-	if ina260 != False:
-		pData['pv'] = data['PV']['power W']
-	else:
-		pData['pv'] = False
-	if ina219 != False:
-		pData['rpi']=data['RPi']['power W']
-	else:
-		pData['rpi']= False
-	pData['load'] = data['CT']['current A'] * 120 #convert CT Irms to W
-	
-	#dict to dataframe
-	pData = pd.DataFrame.from_dict(pData)
+		pData['battery'] = data['Power Station']['total_battery_percent']
+		pData['ac_out'] = data['Power Station']['ac_output_power']
+		pData['ac_in'] = data['Power Station']['ac_input_power']
+		pData['dc_out'] = data['Power Station']['dc_output_power']
+		pData['dc_in'] = data['Power Station']['dc_input_power']
+		pData['r1'] = data['R1']
+		if ina260 != False:
+			pData['pv'] = data['PV']['power W']
+		else:
+			pData['pv'] = False
+		if ina219 != False:
+			pData['rpi']=data['RPi']['power W']
+		else:
+			pData['rpi']= False
+		pData['load'] = data['CT']['current A'] * 120 #convert CT Irms to W
+		
+		#dict to dataframe
+		pData = pd.DataFrame.from_dict(pData)
+	except:
+		print('packaging problems')
+		pData = pd.DataFrame()
 
 	return pData
 
